65a643555782919bcc5602e0a85946b697ea618c
Anas-Mahdy <anasmahdy471@gmail.com>
2025-06-22 13:53:50 +0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const person = {
-    id : 456847
-    firstName : "Anas"
-    address : "cairo , Egypt"
-    hobbies : ["football","tennis"]
+    id : 456847,
+    firstName : "Anas",
+    address : {
+        city : "Cairo",
+        country : "Egypt"
+    },
+    hobbies : ["football","tennis"],
+    sayHi : function(){
+        console.log("hi");    
+    },
+   
+    m : function(){
+        t = 5,
+        g = 6,
+        console.log(t == g);
+        {
+            h = 7
+            l = 8
+            console.log(t + g + h +l);    
+        }
+    }
+    
 }
+person.firstName = "islam"
 console.log(person);
+person.sayHi()
+person.m()
+
